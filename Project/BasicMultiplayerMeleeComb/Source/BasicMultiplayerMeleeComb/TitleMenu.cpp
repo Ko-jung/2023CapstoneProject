b4738f8dd63605c1c0a3bdc4c0ebc2d4f7a21a88
Baek Jong Hwa <somang00915@tukorea.ac.kr>
2023-10-19 00:53:36 +0900

    프로토타입용 서버 연결 UI 제작

--- Project/BasicMultiplayerMeleeComb/Source/BasicMultiplayerMeleeComb/TitleMenu.cpp
+++ Project/BasicMultiplayerMeleeComb/Source/BasicMultiplayerMeleeComb/TitleMenu.cpp
@@ -2,4 +2,57 @@
 
 
 #include "TitleMenu.h"
+#include "Blueprint/Userwidget.h"
 
+#include "Components/Button.h"
+#include "Components/WidgetSwitcher.h"
+#include "Engine/GameInstance.h"
+
+bool UTitleMenu::Initialize()
+{
+	bool Success = Super::Initialize();
+	if (!Success) return false;
+
+	if (!HostButton) return false;
+
+	HostButton->OnClicked.AddDynamic(this, &UTitleMenu::HostServer);
+	JoinButton->OnClicked.AddDynamic(this, &UTitleMenu::JoinServer);
+	QuitButton->OnClicked.AddDynamic(this, &UTitleMenu::Quit);
+
+	CancelButton->OnClicked.AddDynamic(this, &UTitleMenu::Cencel);
+	EnterButton->OnClicked.AddDynamic(this, &UTitleMenu::EnterServer);
+
+	return true;
+}
+
+void UTitleMenu::HostServer()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Called Host Server"));
+}
+
+void UTitleMenu::JoinServer()
+{
+	Switcher->SetActiveWidgetIndex(1);
+}
+
+void UTitleMenu::Quit()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Called Quit"));
+	//FGenericPlatformMisc::RequestExit(false);
+	
+
+	auto World = GetWorld();
+	auto Controller = World->GetFirstPlayerController();
+
+	Controller->ConsoleCommand("quit");
+}
+
+void UTitleMenu::EnterServer()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Called Enter Server"));
+}
+
+void UTitleMenu::Cencel()
+{
+	Switcher->SetActiveWidgetIndex(0);
+}
--- Project/BasicMultiplayerMeleeComb/Source/BasicMultiplayerMeleeComb/TitleMenu.h
+++ Project/BasicMultiplayerMeleeComb/Source/BasicMultiplayerMeleeComb/TitleMenu.h
@@ -13,5 +13,41 @@ UCLASS()
 class BASICMULTIPLAYERMELEECOMB_API UTitleMenu : public UUserWidget
 {
 	GENERATED_BODY()
+
+protected:
+	virtual bool Initialize() override;
 	
+private:
+	UPROPERTY(meta = (BindWidget))
+	class UButton* HostButton;
+
+	UPROPERTY(meta = (BindWidget))
+	class UButton* JoinButton;
+
+	UPROPERTY(meta = (BindWidget))
+	class UButton* QuitButton;
+
+	UPROPERTY(meta = (BindWidget))
+	class UButton* CancelButton;
+
+	UPROPERTY(meta = (BindWidget))
+	class UButton* EnterButton;
+
+	UPROPERTY(meta = (BindWidget))
+	class UWidgetSwitcher* Switcher;
+
+	UFUNCTION()
+	void HostServer();
+
+	UFUNCTION()
+	void JoinServer();
+
+	UFUNCTION()
+	void Quit();
+
+	UFUNCTION()
+	void EnterServer();
+
+	UFUNCTION()
+	void Cencel();
 };

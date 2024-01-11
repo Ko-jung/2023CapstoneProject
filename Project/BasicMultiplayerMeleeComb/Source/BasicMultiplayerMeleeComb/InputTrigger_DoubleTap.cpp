a7f514105ca8948f5b2da8ee857e13a574150a26
Lee Yoon Seok <kjggr123@gmail.com>
2024-01-11 22:13:29 +0900

    JetPack Component 오류 수정

---
+++ Project/BasicMultiplayerMeleeComb/Source/BasicMultiplayerMeleeComb/InputTrigger_DoubleTap.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "InputTrigger_DoubleTap.h"
+
+InputTrigger_DoubleTap::InputTrigger_DoubleTap()
+{
+}
+
+InputTrigger_DoubleTap::~InputTrigger_DoubleTap()
+{
+}
---
+++ Project/BasicMultiplayerMeleeComb/Source/BasicMultiplayerMeleeComb/InputTrigger_DoubleTap.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class BASICMULTIPLAYERMELEECOMB_API InputTrigger_DoubleTap
+{
+public:
+	InputTrigger_DoubleTap();
+	~InputTrigger_DoubleTap();
+};

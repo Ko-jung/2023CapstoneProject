904ee0f6be488afd3990e54d5ca2bbed1d74c2f3
Lee Yoon Seok <kjggr123@gmail.com>
2024-04-14 21:19:40 +0900

    애니메이션 템플릿 사망시에 대한 애니메이션 연결 및 Assassin 캐릭터에 대한 경직/다운 애니메이션 분리

--- Project/Skyscraper/Source/Skyscraper/MainGame/Actor/Character/AssassinCharacter.cpp
+++ Project/Skyscraper/Source/Skyscraper/MainGame/Actor/Character/AssassinCharacter.cpp
@@ -9,7 +9,7 @@ AAssassinCharacter::AAssassinCharacter()
 		static ConstructorHelpers::FObjectFinder<USkeletalMesh> MeshAsset(TEXT("/Script/Engine.SkeletalMesh'/Game/2016180023/character/4_assassin/assassin.assassin'"));
 		GetMesh()->SetSkeletalMesh(MeshAsset.Object);
 		// == Find and set AnimBlueprint (TEMP, Refactor to c++ later)
-		static ConstructorHelpers::FClassFinder<UAnimInstance> AnimBPAsset(TEXT("/Script/Engine.AnimBlueprint'/Game/2019180031/MainGame/Animation/Assassin/Assassin/ABP_Assassin.ABP_Assassin_C'"));
+		static ConstructorHelpers::FClassFinder<UAnimInstance> AnimBPAsset(TEXT("/Script/Engine.AnimBlueprint'/Game/2019180031/MainGame/Animation/Assassin/ABP_Assassin.ABP_Assassin_C'"));
 		GetMesh()->SetAnimClass(AnimBPAsset.Class);
 	}
 
@@ -26,12 +26,11 @@ AAssassinCharacter::AAssassinCharacter()
 		//*CharacterAnimMontages.Find(ECharacterAnimMontage::ECAM_KatanaAttack4) = AM_Katana4Ref.Object;
 		
 		//Rifle
-		//const ConstructorHelpers::FObjectFinder<UAnimMontage> AM_RifleFireRef(TEXT("/Script/Engine.AnimMontage'/Game/2019180031/MainGame/Animation/Assassin/Assassin/Combat/Range/Rifle/AM_Assassin_AR_Fire.AM_Assassin_AR_Fire'"));
-		const ConstructorHelpers::FObjectFinder<UAnimMontage> AM_RifleFireRef(TEXT("/Script/Engine.AnimMontage'/Game/2019180031/MainGame/Animation/Assassin/Assassin/Combat/Range/Rifle/AM_Assassin_AR_Fire.AM_Assassin_AR_Fire'"));
+		const ConstructorHelpers::FObjectFinder<UAnimMontage> AM_RifleFireRef(TEXT("/Script/Engine.AnimMontage'/Game/2019180031/MainGame/Animation/Assassin/Combat/Range/Rifle/AM_Assassin_AR_Fire.AM_Assassin_AR_Fire'"));
 		*CharacterAnimMontages.Find(ECharacterAnimMontage::ECAM_RifleFire) = AM_RifleFireRef.Object;
 		const ConstructorHelpers::FObjectFinder<UAnimMontage> AM_RifleReadyFireRef(TEXT(""));
 		*CharacterAnimMontages.Find(ECharacterAnimMontage::ECAM_RifleReadyFire) = AM_RifleReadyFireRef.Object;
-		const ConstructorHelpers::FObjectFinder<UAnimMontage> AM_RifleReloadRef(TEXT("/Script/Engine.AnimMontage'/Game/2019180031/MainGame/Animation/Assassin/Assassin/Combat/Range/Rifle/AM_Assassin_AR_Reload.AM_Assassin_AR_Reload'"));
+		const ConstructorHelpers::FObjectFinder<UAnimMontage> AM_RifleReloadRef(TEXT("/Script/Engine.AnimMontage'/Game/2019180031/MainGame/Animation/Assassin/Combat/Range/Rifle/AM_Assassin_AR_Reload.AM_Assassin_AR_Reload'"));
 		*CharacterAnimMontages.Find(ECharacterAnimMontage::ECAM_RifleReload) = AM_RifleReloadRef.Object;
 	}
 }

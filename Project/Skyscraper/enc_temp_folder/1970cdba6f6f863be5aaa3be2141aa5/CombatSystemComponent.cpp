d8a740ce346d98110b30c139d40b0c76e54c2b0f
Lee Yoon Seok <kjggr123@gmail.com>
2024-04-14 17:06:59 +0900

    캐릭터에 대한 애니메이션 템플릿 제작 및 어쌔신 캐릭터 연결, katana 근접공격 몽타쥬에 대해서 적용

--- Project/Skyscraper/enc_temp_folder/1970cdba6f6f863be5aaa3be2141aa5/CombatSystemComponent.cpp
+++ Project/Skyscraper/enc_temp_folder/1970cdba6f6f863be5aaa3be2141aa5/CombatSystemComponent.cpp
@@ -30,7 +30,7 @@ UCombatSystemComponent::UCombatSystemComponent()
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = false;
 
-	MeleeSelect = EMeleeSelect::EMS_Katana;
+	MeleeSelect = EMeleeSelect::EMS_Dagger;
 	RangeSelect = ERangeSelect::ERS_Rifle;
 	OwnerCharacter = nullptr;
 	OwnerAnimInstance = nullptr;

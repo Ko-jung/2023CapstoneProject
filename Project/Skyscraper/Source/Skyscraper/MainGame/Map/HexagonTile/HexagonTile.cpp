2cd7bd2c96fb6c49fc961bc93aa9e077ce1ba17d
Lee Yoon Seok <kjggr123@gmail.com>
2024-04-20 22:02:05 +0900

    새로운 설계안을 기반으로 맵 재설계를 위한 브랜치 생성

---
+++ Project/Skyscraper/Source/Skyscraper/MainGame/Map/HexagonTile/HexagonTile.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HexagonTile.h"
+
+// Sets default values
+AHexagonTile::AHexagonTile()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+// Called when the game starts or when spawned
+void AHexagonTile::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AHexagonTile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+

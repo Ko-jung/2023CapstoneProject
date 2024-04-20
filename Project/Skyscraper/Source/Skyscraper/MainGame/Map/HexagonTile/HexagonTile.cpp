4ccdec52338d1dab338db0275427d5b317749bfa
Lee Yoon Seok <kjggr123@gmail.com>
2024-04-20 23:04:43 +0900

    HexagonTile에 대한 C++ 클래스 생성 및 블루프린트 차일드 액터 생성

--- Project/Skyscraper/Source/Skyscraper/MainGame/Map/HexagonTile/HexagonTile.cpp
+++ Project/Skyscraper/Source/Skyscraper/MainGame/Map/HexagonTile/HexagonTile.cpp
@@ -3,12 +3,98 @@
 
 #include "HexagonTile.h"
 
+#include "Kismet/KismetMathLibrary.h"
+
 // Sets default values
 AHexagonTile::AHexagonTile()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// bCanEverTick
 	PrimaryActorTick.bCanEverTick = false;
 
+	// 변수값 초기화
+	offset = 3465.0f;
+
+	// 타일 static mesh 로드
+	static ConstructorHelpers::FClassFinder<AActor> SingleHexagonTileClass(TEXT("/Script/Engine.Blueprint'/Game/2019180031/MainGame/Map/HexagonTile/SingleHexagonTile.SingleHexagonTile_C'"));
+	if (!(SingleHexagonTileClass.Succeeded())) return;
+
+	UClass* TileClass = SingleHexagonTileClass.Class;
+
+	for (int i = 0; i < 37; ++i)
+		Tiles.AddDefaulted();
+
+	{	//중앙 육각 타일 배치
+		Tiles[0] = CreateDefaultSubobject<UChildActorComponent>(TEXT("MiddleTile"));
+		Tiles[0]->SetChildActorClass(TileClass);
+		Tiles[0]->ComponentTags.Add(TEXT("MiddleTile"));
+		CurrentMiddleTile = Tiles[0];
+		SetRootComponent(Tiles[0]);
+	}
+
+
+	int tilecount = 1;
+	for (int angle = 0; angle < 6; ++angle)	// 각도 6부분 타일에 대해
+	{
+		for (int distance = 1; distance < 4; ++distance)	// 중앙 타일로부터의 거리
+		{
+			FString tag = "Section" + FString::FromInt((distance - 2) * -1 + 2);
+			{//60도 방면 위치에 StaticMesh 추가
+				FString name = tag + "_" + FString::FromInt(tilecount);
+				Tiles[tilecount] = CreateDefaultSubobject<UChildActorComponent>(FName(name));
+				Tiles[tilecount]->SetChildActorClass(TileClass);
+				Tiles[tilecount]->SetupAttachment(Tiles[0]);
+				Tiles[tilecount]->SetRelativeLocation(CalculateRelativeLocation(angle, distance));
+				Tiles[tilecount]->ComponentTags.Add(FName(tag));
+				++tilecount;
+			}
+
+			// 60도 방면 사이 위치에 Static Mesh 추가
+			for (int midCount = 2; midCount < distance + 1; ++midCount)
+			{
+				FVector firstLoc = CalculateRelativeLocation(angle, distance);
+				FVector SecondLoc = CalculateRelativeLocation(angle + 1, distance);
+
+				FVector RelatLoc = ((firstLoc - SecondLoc) / (float)distance) * (midCount - 1) + SecondLoc;
+				FString name = tag + "_" + FString::FromInt(tilecount);
+				Tiles[tilecount] = CreateDefaultSubobject<UChildActorComponent>(FName(name));
+				Tiles[tilecount]->SetChildActorClass(TileClass);
+				Tiles[tilecount]->SetupAttachment(Tiles[0]);
+				Tiles[tilecount]->SetRelativeLocation(RelatLoc);
+				Tiles[tilecount]->ComponentTags.Add(FName(tag));
+				++tilecount;
+
+			}
+
+		}
+
+	}
+
+	/*static ConstructorHelpers::FObjectFinder<UBlueprint> BuildingItem(TEXT("/Script/Engine.Blueprint'/Game/2019180031/Blueprints/Map/Building/CodeBuilding.CodeBuilding'"));
+	if (BuildingItem.Object)
+	{
+		BuildingBP = (UClass*)BuildingItem.Object->GeneratedClass;
+	}
+	static ConstructorHelpers::FObjectFinder<UBlueprint> FloatingTileItem(TEXT("/Script/Engine.Blueprint'/Game/2019180031/Blueprints/Map/FloatingTile/CodeFloatingTile.CodeFloatingTile'"));
+	if (FloatingTileItem.Object)
+	{
+		FloatingTileBP = (UClass*)FloatingTileItem.Object->GeneratedClass;
+	}
+	static ConstructorHelpers::FObjectFinder<UBlueprint> GCTileItem(TEXT("/Script/Engine.Blueprint'/Game/2019180031/Blueprints/Map/Tile/Tile_Geometry/GC_Tile.GC_Tile'"));
+	if (GCTileItem.Object)
+	{
+		GC_TileBP = (UClass*)GCTileItem.Object->GeneratedClass;
+	}*/
+}
+
+FVector AHexagonTile::CalculateRelativeLocation(int32 AngleCount, int32 Distance)
+{
+	// 주어진 각도와 거리(구역1~4) 에 따라 적합한 RelativeLocation을 반환하는 함수
+	float TileDistance = offset * Distance;
+
+	float sin = UKismetMathLibrary::DegSin(AngleCount * 60 + 30);
+	float cos = UKismetMathLibrary::DegCos(AngleCount * 60 + 30);
+
+	return FVector(TileDistance * sin, TileDistance * cos, 0.0f);
 }
 
 // Called when the game starts or when spawned
--- Project/Skyscraper/Source/Skyscraper/MainGame/Map/HexagonTile/HexagonTile.h
+++ Project/Skyscraper/Source/Skyscraper/MainGame/Map/HexagonTile/HexagonTile.h
@@ -10,10 +10,53 @@ UCLASS()
 class SKYSCRAPER_API AHexagonTile : public AActor
 {
 	GENERATED_BODY()
-	
+protected:
+	/* 육각 타일 지름 길이 */
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "Default")
+		double offset;
+
+	/* 현재 중간 타일 static mesh component */
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "Default")
+		TObjectPtr<UChildActorComponent> CurrentMiddleTile;
+
+	/* 전체 육각 타일 배열 */
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "Default")
+		TArray<UChildActorComponent*> Tiles;
+
+	/* 육각타일과 그 아래 건물/부유타일에 대한 map */
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "Default")
+		TMap<UStaticMeshComponent*, AActor*> Tile_Actor;
+
+	UPROPERTY(EditAnywhere)
+		TSubclassOf<class AActor> BuildingBP;
+	UPROPERTY(EditAnywhere)
+		TSubclassOf<class AActor> FloatingTileBP;
+	UPROPERTY(EditAnywhere)
+		TSubclassOf<class AActor> GC_TileBP;
 public:	
-	// Sets default values for this actor's properties
+	// 생성자
 	AHexagonTile();
+
+	/* 생성자에서 타일들의 위치 계산해주는 함수 */
+	UFUNCTION(BlueprintPure, Category = "Construct")
+		FVector CalculateRelativeLocation(int32 AngleCount, int32 Distance);
+
+	///* BeginPlay시 진행할 로직 함수 */
+	//UFUNCTION(BlueprintCallable, Category = "Init")
+	//	void InitialSettiongs();
+
+	///* 건물 및 부유타일 생성 함수 */
+	//UFUNCTION(BlueprintCallable, Category = "Init")
+	//	void SpawnBuildingsAndFloatingTiles(int32 SpawnCount, FName TileTag, int32 Floor, int32 FloatingTileCount, FVector MovementOffset);
+
+	///* 붕괴 1단계/2단계 함수 */
+	//UFUNCTION(BlueprintCallable, Category = "Collapse")
+	//	void CollapseLevel1And2(int CollapseLevel);
+
+	///* 붕괴 3단계 함수 */
+	//UFUNCTION(BlueprintCallable, Category = "Collapse")
+	//	void CollapseLevel3();
+
 
 protected:
 	// Called when the game starts or when spawned
@@ -22,5 +65,6 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
 
 };

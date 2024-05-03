de080689b868dc3fdf8fcb58a234dde27da1b918
Lee Yoon Seok <kjggr123@gmail.com>
2024-05-03 22:42:08 +0900

    부메랑 캐릭터 애니메이션 관련 에셋 제작 및 캐릭터 클래스 생성 완료

---
+++ Project/Skyscraper/Source/Skyscraper/MainGame/Actor/Character/BoomerangCharacter.cpp
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BoomerangCharacter.h"
+
+ABoomerangCharacter::ABoomerangCharacter()
+{
+	{ // Set Mesh Character
+		static ConstructorHelpers::FObjectFinder<USkeletalMesh> MeshAsset(TEXT("/Script/Engine.SkeletalMesh'/Game/2016180023/character/3_boomerang/boomerang.boomerang'"));
+		GetMesh()->SetSkeletalMesh(MeshAsset.Object);
+		GetMesh()->SetRelativeLocation(FVector(0.0f, 0.0f, -90.0f));
+		GetMesh()->SetRelativeRotation(FRotator(0.0f, -90.0f, 0.0f));
+		// == Find and set AnimBlueprint (TEMP, Refactor to c++ later)
+		static ConstructorHelpers::FClassFinder<UAnimInstance> AnimBPAsset(TEXT("/Script/Engine.AnimBlueprint'/Game/2019180031/MainGame/Animation/Boomerang/ABP_Boomerang.ABP_Boomerang_C'"));
+		GetMesh()->SetAnimClass(AnimBPAsset.Class);
+	}
+
+
+	{ // == Set Anim Montages
+		// Dagger
+		const ConstructorHelpers::FObjectFinder<UAnimMontage> AM_DaggerRef(TEXT("/Script/Engine.AnimMontage'/Game/2019180031/MainGame/Animation/Boomerang/Combat/Melee/Dagger/AM_Boomerang_DaggerAttack.AM_Boomerang_DaggerAttack'"));
+		*CharacterAnimMontages.Find(ECharacterAnimMontage::ECAM_DaggerAttack) = AM_DaggerRef.Object;
+
+		// Katana
+		const ConstructorHelpers::FObjectFinder<UAnimMontage> AM_KatanaRef(TEXT("/Script/Engine.AnimMontage'/Game/2019180031/MainGame/Animation/Boomerang/Combat/Melee/Katana/AM_Boomerang_KatanaAttack.AM_Boomerang_KatanaAttack'"));
+		*CharacterAnimMontages.Find(ECharacterAnimMontage::ECAM_KatanaAttack) = AM_KatanaRef.Object;
+
+		// GreatSword
+		const ConstructorHelpers::FObjectFinder<UAnimMontage> AM_GreatSwordRef(TEXT("/Script/Engine.AnimMontage'/Game/2019180031/MainGame/Animation/Boomerang/Combat/Melee/Sword/AM_Boomerang_SwordAttack.AM_Boomerang_SwordAttack'"));
+		*CharacterAnimMontages.Find(ECharacterAnimMontage::ECAM_GreatSwordAttack) = AM_GreatSwordRef.Object;
+
+
+		//SMG
+		const ConstructorHelpers::FObjectFinder<UAnimMontage> AM_SMGRef(TEXT("/Script/Engine.AnimMontage'/Game/2019180031/MainGame/Animation/Boomerang/Combat/Range/SMG/AM_Boomerang_SMG.AM_Boomerang_SMG'"));
+		*CharacterAnimMontages.Find(ECharacterAnimMontage::ECAM_SMG) = AM_SMGRef.Object;
+
+		//Rifle
+		const ConstructorHelpers::FObjectFinder<UAnimMontage> AM_RifleRef(TEXT("/Script/Engine.AnimMontage'/Game/2019180031/MainGame/Animation/Boomerang/Combat/Range/Rifle/AM_Boomerang_Rifle.AM_Boomerang_Rifle'"));
+		*CharacterAnimMontages.Find(ECharacterAnimMontage::ECAM_Rifle) = AM_RifleRef.Object;
+
+		//RPG
+		const ConstructorHelpers::FObjectFinder<UAnimMontage> AM_RPGFireRef(TEXT("/Script/Engine.AnimMontage'/Game/2019180031/MainGame/Animation/Boomerang/Combat/Range/RPG/AM_Boomerang_RPG.AM_Boomerang_RPG'"));
+		*CharacterAnimMontages.Find(ECharacterAnimMontage::ECAM_RPG) = AM_RPGFireRef.Object;
+
+		// Stun / Down
+		const ConstructorHelpers::FObjectFinder<UAnimMontage> AM_StunRef(TEXT("/Script/Engine.AnimMontage'/Game/2019180031/MainGame/Animation/Boomerang/Combat/Damaged/AM_Boomerang_Hit.AM_Boomerang_Hit'"));
+		*CharacterAnimMontages.Find(ECharacterAnimMontage::ECAM_Stun) = AM_StunRef.Object;
+
+		const ConstructorHelpers::FObjectFinder<UAnimMontage> AM_DownRef(TEXT("/Script/Engine.AnimMontage'/Game/2019180031/MainGame/Animation/Boomerang/Combat/Damaged/AM_Boomerang_Down.AM_Boomerang_Down'"));
+		*CharacterAnimMontages.Find(ECharacterAnimMontage::ECAM_Down) = AM_DownRef.Object;
+
+		// Death
+		const ConstructorHelpers::FObjectFinder<UAnimMontage> AM_DeathRef(TEXT("/Script/Engine.AnimMontage'/Game/2019180031/MainGame/Animation/Boomerang/Combat/Death/AM_Boomerang_Death.AM_Boomerang_Death'"));
+		*CharacterAnimMontages.Find(ECharacterAnimMontage::ECAM_Death) = AM_DeathRef.Object;
+
+
+	}
+}

e8f9a721eeeb7cf07f18ab5e6fd2ea796de8c113
Lee Yoon Seok <kjggr123@gmail.com>
2024-05-03 20:46:59 +0900

    ShieldCharacter 에 대한 애니메이션 모두 제작 완료 및 캐릭터 클래스 생성 완료

--- Project/Skyscraper/Source/Skyscraper/MainGame/Actor/Character/ShieldCharacter.cpp
+++ Project/Skyscraper/Source/Skyscraper/MainGame/Actor/Character/ShieldCharacter.cpp
@@ -6,17 +6,53 @@
 AShieldCharacter::AShieldCharacter()
 {
 	{ // Set Mesh Character
-		static ConstructorHelpers::FObjectFinder<USkeletalMesh> MeshAsset(TEXT("/Script/Engine.SkeletalMesh'/Game/2019180031/Character/ShieldCharacter/ShieldCharacter.ShieldCharacter'"));
+		static ConstructorHelpers::FObjectFinder<USkeletalMesh> MeshAsset(TEXT("/Script/Engine.SkeletalMesh'/Game/2016180023/character/0_shield/shield.shield'"));
 		GetMesh()->SetSkeletalMesh(MeshAsset.Object);
 		GetMesh()->SetRelativeLocation(FVector(0.0f, 0.0f, -90.0f));
 		GetMesh()->SetRelativeRotation(FRotator(0.0f, -90.0f, 0.0f));
 		// == Find and set AnimBlueprint (TEMP, Refactor to c++ later)
-		static ConstructorHelpers::FClassFinder<UAnimInstance> AnimBPAsset(TEXT("/Script/Engine.AnimBlueprint'/Game/2019180031/Character/ShieldCharacter/ABP_Shield.ABP_Shield_C'"));
+		static ConstructorHelpers::FClassFinder<UAnimInstance> AnimBPAsset(TEXT("/Script/Engine.AnimBlueprint'/Game/2019180031/MainGame/Animation/Shield/ABP_Shield.ABP_Shield_C'"));
 		GetMesh()->SetAnimClass(AnimBPAsset.Class);
 	}
 
-	if(CharacterAnimMontages.IsEmpty())
-	{
-		
+	
+	{ // == Set Anim Montages
+		// Dagger
+		const ConstructorHelpers::FObjectFinder<UAnimMontage> AM_DaggerRef(TEXT("/Script/Engine.AnimMontage'/Game/2019180031/MainGame/Animation/Shield/Combat/Melee/Dagger/AM_Shield_DaggerAttack.AM_Shield_DaggerAttack'"));
+		*CharacterAnimMontages.Find(ECharacterAnimMontage::ECAM_DaggerAttack) = AM_DaggerRef.Object;
+
+		// Katana
+		const ConstructorHelpers::FObjectFinder<UAnimMontage> AM_KatanaRef(TEXT("/Script/Engine.AnimMontage'/Game/2019180031/MainGame/Animation/Shield/Combat/Melee/Katana/AM_Shield_KatanaAttack.AM_Shield_KatanaAttack'"));
+		*CharacterAnimMontages.Find(ECharacterAnimMontage::ECAM_KatanaAttack)= AM_KatanaRef.Object;
+
+		// GreatSword
+		const ConstructorHelpers::FObjectFinder<UAnimMontage> AM_GreatSwordRef(TEXT("/Script/Engine.AnimMontage'/Game/2019180031/MainGame/Animation/Shield/Combat/Melee/Sword/AM_Shield_SwordAttack.AM_Shield_SwordAttack'"));
+		*CharacterAnimMontages.Find(ECharacterAnimMontage::ECAM_GreatSwordAttack) = AM_GreatSwordRef.Object;
+
+
+		//SMG
+		const ConstructorHelpers::FObjectFinder<UAnimMontage> AM_SMGRef(TEXT("/Script/Engine.AnimMontage'/Game/2019180031/MainGame/Animation/Shield/Combat/Range/SMG/AM_Shield_SMG.AM_Shield_SMG'"));
+		*CharacterAnimMontages.Find(ECharacterAnimMontage::ECAM_SMG) = AM_SMGRef.Object;
+
+		//Rifle
+		const ConstructorHelpers::FObjectFinder<UAnimMontage> AM_RifleRef(TEXT("/Script/Engine.AnimMontage'/Game/2019180031/MainGame/Animation/Shield/Combat/Range/Rifle/AM_Shield_Rifle.AM_Shield_Rifle'"));
+		*CharacterAnimMontages.Find(ECharacterAnimMontage::ECAM_Rifle) = AM_RifleRef.Object;
+
+		//RPG
+		const ConstructorHelpers::FObjectFinder<UAnimMontage> AM_RPGFireRef(TEXT("/Script/Engine.AnimMontage'/Game/2019180031/MainGame/Animation/Shield/Combat/Range/RPG/AM_Shield_RPG.AM_Shield_RPG'"));
+		*CharacterAnimMontages.Find(ECharacterAnimMontage::ECAM_RPG) = AM_RPGFireRef.Object;
+
+		// Stun / Down
+		const ConstructorHelpers::FObjectFinder<UAnimMontage> AM_StunRef(TEXT("/Script/Engine.AnimMontage'/Game/2019180031/MainGame/Animation/Shield/Combat/Damaged/AM_Shield_Hit.AM_Shield_Hit'"));
+		*CharacterAnimMontages.Find(ECharacterAnimMontage::ECAM_Stun) = AM_StunRef.Object;
+
+		const ConstructorHelpers::FObjectFinder<UAnimMontage> AM_DownRef(TEXT("/Script/Engine.AnimMontage'/Game/2019180031/MainGame/Animation/Shield/Combat/Damaged/AM_Shield_Down.AM_Shield_Down'"));
+		*CharacterAnimMontages.Find(ECharacterAnimMontage::ECAM_Down) = AM_DownRef.Object;
+
+		// Death
+		const ConstructorHelpers::FObjectFinder<UAnimMontage> AM_DeathRef(TEXT("/Script/Engine.AnimMontage'/Game/2019180031/MainGame/Animation/Shield/Combat/Death/AM_Shield_Death.AM_Shield_Death'"));
+		*CharacterAnimMontages.Find(ECharacterAnimMontage::ECAM_Death) = AM_DeathRef.Object;
+
+
 	}
 }

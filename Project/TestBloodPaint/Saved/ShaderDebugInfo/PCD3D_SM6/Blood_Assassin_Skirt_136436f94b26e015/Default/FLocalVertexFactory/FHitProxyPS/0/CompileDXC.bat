3d2a0b8375f477ce383bf70519d951b87ec4327e
Lee Yoon Seok <kjggr123@gmail.com>
2024-06-01 00:56:08 +0900

    마천루 캐릭터 -> 각 테스트 캐릭터 생성 및 머테리얼 내 MaterialFunction을 통해 MF_Blood 함수 연결

---
+++ Project/TestBloodPaint/Saved/ShaderDebugInfo/PCD3D_SM6/Blood_Assassin_Skirt_136436f94b26e015/Default/FLocalVertexFactory/FHitProxyPS/0/CompileDXC.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+SET DXC="C:\Users\Administrator\Desktop\Unreal\UnrealEngine\Engine\Binaries\ThirdParty\ShaderConductor\Win64\dxc.exe"
+IF NOT EXIST %DXC% (
+	ECHO Couldn't find dxc.exe under "C:\Users\Administrator\Desktop\Unreal\UnrealEngine\Engine\Binaries\ThirdParty\ShaderConductor\Win64"
+	GOTO :END
+)
+%DXC% -Zpr -O3 -auto-binding-space 0 -Wno-parentheses-equality -disable-lifetime-markers -T ps_6_6 -E Main -Fc HitProxyPixelShader.d3dasm -Fo HitProxyPixelShader.dxil HitProxyPixelShader
+:END
+PAUSE

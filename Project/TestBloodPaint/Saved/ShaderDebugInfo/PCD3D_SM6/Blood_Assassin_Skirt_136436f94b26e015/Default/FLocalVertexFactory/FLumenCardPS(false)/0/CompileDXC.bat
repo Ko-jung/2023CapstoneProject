f2418c98fcf787584570ae240454a4d71230ff40
Lee Yoon Seok <kjggr123@gmail.com>
2024-06-04 03:02:10 +0900

    테스트 프로젝트 제거

--- Project/TestBloodPaint/Saved/ShaderDebugInfo/PCD3D_SM6/Blood_Assassin_Skirt_136436f94b26e015/Default/FLocalVertexFactory/FLumenCardPS(false)/0/CompileDXC.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-SET DXC="C:\Users\Administrator\Desktop\Unreal\UnrealEngine\Engine\Binaries\ThirdParty\ShaderConductor\Win64\dxc.exe"
-IF NOT EXIST %DXC% (
-	ECHO Couldn't find dxc.exe under "C:\Users\Administrator\Desktop\Unreal\UnrealEngine\Engine\Binaries\ThirdParty\ShaderConductor\Win64"
-	GOTO :END
-)
-%DXC% -Zpr -O3 -auto-binding-space 0 -Wno-parentheses-equality -disable-lifetime-markers -T ps_6_6 -E Main -Fc LumenCardPixelShader.d3dasm -Fo LumenCardPixelShader.dxil LumenCardPixelShader
-:END
-PAUSE
